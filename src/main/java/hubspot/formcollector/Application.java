430797f46ca2e91efca1d8803e2ee85b3b83d58a
gregwhitaker <gwhitake@gmail.com>
2018-12-25 10:58:56 -0600

    Doing work

--- src/main/java/hubspot/formcollector/Application.java
+++ src/main/java/hubspot/formcollector/Application.java
@@ -15,13 +15,24 @@
  */
 package hubspot.formcollector;
 
+import hubspot.formcollector.handler.FormHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> formRouter(FormHandler handler) {
+        return RouterFunctions.route(RequestPredicates.POST("*"), handler::formSubmit);
     }
 }
--- src/main/java/hubspot/formcollector/handler/FormHandler.java
+++ src/main/java/hubspot/formcollector/handler/FormHandler.java
@@ -18,10 +18,15 @@ package hubspot.formcollector.handler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @Component
 public class FormHandler {
     private static final Logger LOG = LoggerFactory.getLogger(FormHandler.class);
 
-    
+    public Mono<ServerResponse> formSubmit(ServerRequest request) {
+        return null;
+    }
 }
